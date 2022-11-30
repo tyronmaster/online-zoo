4c2115b866c6a9b3ad37a6975afe86bc5fad0eca
tyronmaster <degonski.art@gmail.com>
2022-11-30 17:56:23 +0300

    fix: correct burger menu behaviour

--- pages/main/burger.js
+++ pages/main/burger.js
@@ -1,20 +1,22 @@
 const body = document.querySelector("body");
 const burger = document.querySelector(".burger");
-const header = document.querySelector(".header__inner");
+const headerInner = document.querySelector(".header__inner");
+const header = document.querySelector(".header");
 
 burger.addEventListener("click", (e) => {
     classToggle(e.currentTarget, "active"); // === classToggle(burger, "active");
+    classToggle(headerInner, "active");
     classToggle(header, "active");
     classToggle(body, "lock");
 });
 
 header.addEventListener("click", (e) => {
-    if(e.target.classList.contains("header__inner")){
+    if(e.target.classList.contains("header")){
         classToggle(burger, "active");
+        classToggle(headerInner, "active");
         classToggle(header, "active");
         classToggle(body, "lock");
     }
-
 });
 
 function classToggle(element, className) {
